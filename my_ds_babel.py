6fd02e353b1e4b10bd262287e724fef4cddc1c6f
boburergashew <153498795+boburergashew@users.noreply.github.com>
2024-02-28 01:12:41 +0500

    Fixedbugs

--- my_ds_babel.py
+++ my_ds_babel.py
@@ -1,18 +1,17 @@
 import sqlite3
 import pandas as pd 
 
-def sql_to_csv(database, table_name):
-  connection = sqlite3.connect(database)
-  select = f"SELECT * FROM {table_name};"
-  request = pd.read_sql_query(select, connection)
-  request.to_csv("list_fault_lines.csv", index = False)
-  with open("list_fault_lines.csv", "r") as file:
-    all_results = file.read()
-  return all_results[:-1]
-
-
 def csv_to_sql(csv_context, database, table_name):
-    connection = sql.connect(database)
+    connection = sqlite3.connect(database)
     dataframe = pd.read_csv(csv_context)
     dataframe.to_sql(name=table_name, con=connection, index=False)
     connection.close()
+
+def sql_to_csv(database, table_name):
+    connection = sqlite3.connect(database)
+    select = f"SELECT * FROM {table_name};"
+    request = pd.read_sql_query(select, connection)
+    request.to_csv("list_fault_lines.csv", index=False)
+    with open("list_fault_lines.csv", "r") as file:
+        all_results = file.read()
+    return all_results[:-1]
